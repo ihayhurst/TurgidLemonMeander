01fe6eb4358a77c2015f77ae51f7e94254a474f8
ihayhurst <29225030+ihayhurst@users.noreply.github.com>
2020-04-17 20:47:42 +0100

    linting

--- graph.py
+++ graph.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import matplotlib as mpl
-mpl.use('agg')
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
@@ -9,28 +8,31 @@ import config
 from re import split
 from scipy.interpolate import make_interp_spline
 
-DT_FORMAT       = '%Y/%m/%d-%H:%M'      #format used on the cli
-LOG_DT_FORMAT = config.log_date_format  #set in config.py to match the format used in the log file
+mpl.use('agg')
+DT_FORMAT       = '%Y/%m/%d-%H:%M'      # format used on the cli
+LOG_DT_FORMAT = config.log_date_format  # set in config.py to match the format used in the log file
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
 APP_STATIC = os.path.join(APP_ROOT, 'static')
-area_name = config.area_name 
+area_name = config.area_name
+
 
 def generateGraph(reading_count, area_name):
     '''Wrapper for drawgraph called from '''
-    kwargs = {'tailmode' : True, 'text' : True}
+    kwargs = {'tailmode': True, 'text': True}
     args   = {reading_count}
     filename = os.path.join(APP_ROOT, 'hpt.log')
     if len(open(filename, encoding="utf-8").readlines()) < reading_count:
-      print('Not enough lines in logfile, aborting\n')
-      plt.figure()
-      plt.savefig('hpt.png')
-      plt.clf()
-      plt.close('all')
-      return
+        print('Not enough lines in logfile, aborting\n')
+        plt.figure()
+        plt.savefig('hpt.png')
+        plt.clf()
+        plt.close('all')
+        return
     x, y, h, p   = readValues(*args, **kwargs)
-    return(drawGraph(x,y,h,p, area_name, **kwargs))
+    return(drawGraph(x, y, h, p, area_name, **kwargs))
 
-def drawGraph(x,y,h,p, area_name, **kwargs):
+
+def drawGraph(x, y, h, p, area_name, **kwargs):
     x2 = mdates.date2num(x)
     x_sm = np.array(x2)
     x_smooth = np.linspace(x_sm.min(), x_sm.max(), 200)
@@ -50,14 +52,14 @@ def drawGraph(x,y,h,p, area_name, **kwargs):
     ax.grid(which='major', axis='x', color='grey')
     hh.spines["right"].set_position(("axes", 1.2))
 
-    plt.plot([],[])
+    plt.plot([], [])
     x_smooth_dt = mdates.num2date(x_smooth)
-    #ax.xaxis.set_major_formatter(mdates.DateFormatter('%b-%d %H:%M'))
+    # ax.xaxis.set_major_formatter(mdates.DateFormatter('%b-%d %H:%M'))
     maFmt = mdates.DateFormatter('%b-%d')
     miFmt = mdates.DateFormatter('%H:%M')
     ax.xaxis.set_major_formatter(maFmt)
     ax.xaxis.set_minor_formatter(miFmt)
-    for label in ax.xaxis.get_minorticklabels()[::2]: # show every other minor label
+    for label in ax.xaxis.get_minorticklabels()[::2]:  # show every other minor label
         label.set_visible(True)
     ax.tick_params(axis='both', which='major', labelsize=10)
 
@@ -79,32 +81,32 @@ def drawGraph(x,y,h,p, area_name, **kwargs):
         pic_IObytes.seek(0)
         pic_hash = base64.b64encode(pic_IObytes.read())
         print("Graph created in base64 encoding")
-        return  pic_hash
+        return pic_hash
 
-    else:  
+    else:
         plt.savefig('graph.png', format='png')
         print('Created graph\n')
         plt.clf()
         plt.close('all')
-    return 
+    return
+
 
 def readValues(*args, **kwargs):
     '''for key, value in kwargs.items():     #Debug
         print ("%s == %s" %(key, value)) #Debug
     '''
-    print("From: ",kwargs.get('from_date'))
-    print("To: ",kwargs.get('to_date')) 
+    print("From: ", kwargs.get('from_date'))
+    print("To: ", kwargs.get('to_date'))
 
-    if kwargs.get('lines') != None:
+    if kwargs.get('lines') is not None:
         reading_count = kwargs.get('lines')
     else:
         reading_count = args[0]
 
-
-    x=[] #Datetime
-    y=[] #Temperature
-    h=[] #Humidity 
-    p=[] #Presure
+    x = []  # Datetime
+    y = []  # Temperature
+    h = []  # Humidity
+    p = []  # Presure
     x.clear()
     y.clear()
     h.clear()
@@ -115,19 +117,19 @@ def readValues(*args, **kwargs):
     except:
         tailmode = True
     if not tailmode:
-        from_dt = date_to_dt(kwargs.get('from_date'),DT_FORMAT)
-        to_dt = date_to_dt(kwargs.get('to_date'),DT_FORMAT)
+        from_dt = date_to_dt(kwargs.get('from_date'), DT_FORMAT)
+        to_dt = date_to_dt(kwargs.get('to_date'), DT_FORMAT)
 
     filename = os.path.join(APP_ROOT, 'hpt.log')
     with open(filename, 'r', encoding="utf-8") as f:
         if tailmode:
-            taildata = f.readlines() [-reading_count:]
+            taildata = f.readlines()[-reading_count:]
         else:
             taildata = f.readlines()
         for line in taildata:
             line = line.translate({ord(i): None for i in '[]'})
             data = split(" ", line)
-            temp, humidity, pressure =  float(data[2]), float(data[3]), float(data[4])
+            temp, humidity, pressure = float(data[2]), float(data[3]), float(data[4])
             dt = f'{data[0]} {data[1]}'
             dt = date_to_dt(dt, LOG_DT_FORMAT)
             if tailmode:
@@ -141,31 +143,33 @@ def readValues(*args, **kwargs):
                     y.append(temp)
                     h.append(humidity)
                     p.append(pressure)
-        return x,y,h,p
+        return x, y, h, p
+
 
 def cmd_args(args=None):
     parser = argparse.ArgumentParser("Graph.py charts range of times from a temperature log")
 
     parser.add_argument('-l', '--lines',  type=int, dest='lines',
-                    help='Number of tailing log lines to plot')
+                        help='Number of tailing log lines to plot')
     parser.add_argument('-s', '--start',  dest='start',
-                    help='Start date YYYY/MM/DD-HH:MM')
+                        help='Start date YYYY/MM/DD-HH:MM')
     parser.add_argument('-e', '--end',  dest='end',
-                    help='End   date YYYY/MM/DD-HH:MM')
+                        help='End   date YYYY/MM/DD-HH:MM')
     parser.add_argument('-d', '--dur',  dest='dur',
-                    help='Duration: Hours, Days, Weeks,  e.g. 2W for 2 weeks')
+                        help='Duration: Hours, Days, Weeks,  e.g. 2W for 2 weeks')
     parser.add_argument('-t', '--text',  dest='text',
-                    help='Output graphic as base64 encoded text')
+                        help='Output graphic as base64 encoded text')
 
     opt = parser.parse_args(args)
 
     return opt
 
+
 def parse_duration(duration):
-    hours = datetime.timedelta(hours = 1)
-    days = datetime.timedelta(days = 1)
-    weeks = datetime.timedelta(weeks = 1)
-    fields = split(r'(\d+)',duration)
+    hours = datetime.timedelta(hours=1)
+    days = datetime.timedelta(days=1)
+    weeks = datetime.timedelta(weeks=1)
+    fields = split(r'(\d+)', duration)
     duration = int(fields[1])
     if fields[2][:1].upper() == 'H':
         duration_td = duration * hours
@@ -175,85 +179,89 @@ def parse_duration(duration):
         duration_td = duration * weeks
     else:
         raise ValueError
-    
+
     return duration_td
+
 
 def date_to_dt(datestring, FORMAT):
     dateasdt = datetime.datetime.strptime(datestring, FORMAT)
     return dateasdt
 
+
 def dt_to_date(dateasdt, FORMAT):
     datestring = datetime.datetime.strftime(dateasdt, FORMAT)
     return datestring
+
 
 def main(args=None):
     opt = cmd_args(args)
     kwargs = {}
 
-    if opt.dur and opt.start and opt.end: #Assume start and range ignore end
-        print("All three madness") #Debug
-        print("Duration",opt.dur)
+    if opt.dur and opt.start and opt.end:  # Assume start and range ignore end
+        print("All three madness")  # Debug
+        print("Duration", opt.dur)
         duration = parse_duration(opt.dur)
         opt.end_dt = date_to_dt(opt.start, DT_FORMAT)+duration
         opt.end = opt.end_dt.strftime(DT_FORMAT)
 
-    if opt.dur and opt.start and not opt.end: #Start and range 
-        print("Start date and duration") #Debug
-        print("Duration",opt.dur)
+    if opt.dur and opt.start and not opt.end:  # Start and range
+        print("Start date and duration")  # Debug
+        print("Duration", opt.dur)
         duration = parse_duration(opt.dur)
-        opt.end_dt = date_to_dt(opt.start, DT_FORMAT)+duration
+        opt.end_dt = date_to_dt(opt.start, DT_FORMAT) + duration
         opt.end = opt.end_dt.strftime(DT_FORMAT)
 
-    if opt.dur and not opt.start and opt.end: #Range before enddate
-        print("End date and duration") #Debug
+    if opt.dur and not opt.start and opt.end:  # Range before enddate
+        print("End date and duration")  # Debug
         duration = parse_duration(opt.dur)
-        opt.start_dt = date_to_dt(opt.end, DT_FORMAT)-duration
+        opt.start_dt = date_to_dt(opt.end, DT_FORMAT) - duration
         opt.start = opt.start_dt.strftime(DT_FORMAT)
-        
-    if opt.dur and not opt.start and not opt.end: #tailmode with range
-        print("End of log back by duratiion") #Debug
+
+    if opt.dur and not opt.start and not opt.end:  # tailmode with range
+        print("End of log back by duratiion")  # Debug
         duration = parse_duration(opt.dur)
         opt.end_dt = datetime.datetime.now()
-        opt.end = dt_to_date (opt.end_dt, DT_FORMAT)
-        opt.start_dt = date_to_dt(opt.end, DT_FORMAT)-duration
+        opt.end = dt_to_date(opt.end_dt, DT_FORMAT)
+        opt.start_dt = date_to_dt(opt.end, DT_FORMAT) - duration
         opt.start = opt.start_dt.strftime(DT_FORMAT)
 
-    if not opt.dur and opt.start and opt.end: #Date range
-        print("Start date and end date") #Debug
-        if date_to_dt(opt.start, DT_FORMAT) >date_to_dt(opt.end, DT_FORMAT): # End before start so swap
+    if not opt.dur and opt.start and opt.end:  # Date range
+        print("Start date and end date")  # Debug
+        if date_to_dt(opt.start, DT_FORMAT) > date_to_dt(opt.end, DT_FORMAT):  # End before start so swap
             opt.start, opt.end = opt.end, opt.start
 
-    if not opt.dur and opt.start and not opt.end: #Start Date only - from start date to end
-        print("Start Date to end of log ") #Debug
+    if not opt.dur and opt.start and not opt.end:  # Start Date only - from start date to end
+        print("Start Date to end of log ")  # Debug
         opt.end_dt = datetime.datetime.now()
         opt.end = opt.end_dt.strftime(DT_FORMAT)
 
-    if not opt.dur and not opt.start and opt.end: #End Date only - from end date to start
-        print("End date back to the dawn of time (or the log at least) ") #Debug
+    if not opt.dur and not opt.start and opt.end:  # End Date only - from end date to start
+        print("End date back to the dawn of time (or the log at least) ")  # Debug
         opt.start_dt = datetime.date(1970, 1, 1)
         opt.start = opt.start_dt.strftime(DT_FORMAT)
 
-    if opt.lines != None: #tailmode with lines
-        print("tail back number of lines") #Debug
-        kwargs={'tailmode': True, 'lines': opt.lines, **kwargs}
+    if opt.lines is not None:  # tailmode with lines
+        print("tail back number of lines")  # Debug
+        kwargs = {'tailmode': True, 'lines': opt.lines, **kwargs}
 
-    if not opt.lines and not opt.dur and not opt.start and not opt.end: #tailmode with lines (none set so using 12)
-        kwargs={'tailmode': True, 'lines': 12}
+    if not opt.lines and not opt.dur and not opt.start and not opt.end:  # tailmode with lines (none set so using 12)
+        kwargs = {'tailmode': True, 'lines': 12}
 
     if not opt.lines:
         print("Not Tailmode")
-        kwargs={'tailmode': False, 'from_date': opt.start, 'to_date': opt.end, **kwargs}
-    
-    if opt.text != None:
-        print("Base64 encoded png output")
-        kwargs={'text': True, **kwargs}
+        kwargs = {'tailmode': False, 'from_date': opt.start, 'to_date': opt.end, **kwargs}
 
-    if not opt.text:    
+    if opt.text is not None:
+        print("Base64 encoded png output")
+        kwargs = {'text': True, **kwargs}
+
+    if not opt.text:
         print("png output file")
-        kwargs={'text': False, **kwargs}
+        kwargs = {'text': False, **kwargs}
 
     x, y, h, p  = readValues(*args, **kwargs)
-    drawGraph(x,y,h,p, area_name, **kwargs)
+    drawGraph(x, y, h, p, area_name, **kwargs)
+
 
 if __name__ == '__main__':
     try:
