2c4fb504ff91266753e496b4909448fcf50786b1
ihayhurst <29225030+ihayhurst@users.noreply.github.com>
2020-11-01 23:58:45 +0000

    enable load config object

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -8,16 +8,10 @@ from .moa.views import moa
 
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
-
-    # app.config.from_object('config.Production')
+    app.config.from_object('config.Production')
 
     with app.app_context():
-        # from . import db
-        # db.init_app(app)
         app.register_blueprint(website, url_prefix="/")
         app.register_blueprint(moa, url_prefix="/moa")
-
         # Initialise Globals
-        # conn_str = f"{app.config['DATABASE_USER']}, {app.config['DATABASE_PASSWORD']}, {dsn}, encoding='UTF-8'"
-        # print(f"Connection = {conn_str}")
     return app
